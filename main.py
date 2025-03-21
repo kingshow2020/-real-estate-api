e109d67e68cf53b14fb1342b20a381b06620a23d
Azzam Almutairi <azzammajed@gmail.com>
2025-03-21 11:21:30 +0300

    أول نسخة من API العقار

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class RealEstateInput(BaseModel):
+    area: float
+    deposit: float
+    opening_price: float
+
+@app.post("/predict")
+def predict_price(data: RealEstateInput):
+    predicted_price = data.opening_price * 1.37
+    return {"predicted_price": predicted_price}
