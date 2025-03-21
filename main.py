9ca10a9d7e67b1aaafee572187ceb28176627065
Azzam Almutairi <azzammajed@gmail.com>
2025-03-21 19:26:01 +0300

    تعديل main.py وإضافة /predict

--- main.py
+++ main.py
@@ -12,3 +12,4 @@ class RealEstateInput(BaseModel):
 def predict_price(data: RealEstateInput):
     predicted_price = data.opening_price * 1.37
     return {"predicted_price": predicted_price}
+#اختبار
